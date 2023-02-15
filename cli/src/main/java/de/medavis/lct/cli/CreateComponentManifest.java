6554da598c99b32105c453c08ee454f992450908
Martin Leim <martin.leim@medavis.de>
2023-02-15 16:00:25 +0100

    Made working. Add license headers

--- cli/src/main/java/de/medavis/lct/cli/CreateComponentManifest.java
+++ cli/src/main/java/de/medavis/lct/cli/CreateComponentManifest.java
@@ -1,3 +1,22 @@
+/*-
+ * #%L
+ * License Compliance Tool - Command Line Interface
+ * %%
+ * Copyright (C) 2022 - 2023 medavis GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package de.medavis.lct.cli;
 
 import java.io.File;
@@ -19,11 +38,11 @@ import de.medavis.lct.core.outputter.FreemarkerOutputter;
 @Command(name = "component-manifest", description = "Create component manifest")
 class CreateComponentManifest implements Callable<Void> {
 
-    @Option(names = "--in, -i")
+    @Option(names = {"--in", "-i"}, required = true)
     private File inputPath;
-    @Option(names = "--out, -o")
+    @Option(names = {"--out", "-o"}, required = true)
     private File outputFile;
-    @Option(names = "--template, -t")
+    @Option(names = {"--template", "-t"})
     private String template;
     @Mixin
     private ConfigurationOptions configurationOptions;
