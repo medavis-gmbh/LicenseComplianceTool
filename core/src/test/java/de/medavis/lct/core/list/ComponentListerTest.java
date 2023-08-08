2269f6c4a52d91adf42286f64d08881a9481ddba
Martin Leim <martin.leim@medavis.de>
2023-08-08 07:50:54 +0200

    Fix assertion order

--- core/src/test/java/de/medavis/lct/core/list/ComponentListerTest.java
+++ core/src/test/java/de/medavis/lct/core/list/ComponentListerTest.java
@@ -65,10 +65,10 @@ class ComponentListerTest {
     void ignoreEmptyGroup() {
         assertThat(executeTest("metadata-empty", "license-empty", "licensemapping-empty", "test-bom-depWithoutGroup"))
                 .containsExactly(
-                        new ComponentData("dep-nogroup", "1.0.0", null, Set.of(
+                        new ComponentData("dep-emptygroup", "2.0.0", null, Set.of(
                                 License.dynamic("EPL-1.0", null)
                         ), Collections.emptySet()),
-                        new ComponentData("dep-emptygroup", "2.0.0", null, Set.of(
+                        new ComponentData("dep-nogroup", "1.0.0", null, Set.of(
                                 License.dynamic("EPL-1.0", null)
                         ), Collections.emptySet())
                 );
