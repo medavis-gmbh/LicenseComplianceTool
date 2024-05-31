7c757d1a2f77f1d64a1cfc30bab509ad6bb9179b
Carsten Rambow <dev@elomagic.de>
2024-05-31 09:01:04 +0200

    Create target directory

--- core/src/main/java/de/medavis/lct/core/patcher/BomPatcher.java
+++ core/src/main/java/de/medavis/lct/core/patcher/BomPatcher.java
@@ -96,6 +96,7 @@ public class BomPatcher {
             boolean result = patch(in, out);
 
             LOGGER.info("Writing patched file '{}'", targetFile);
+            Files.createDirectories(targetFile.getParent());
             Files.writeString(targetFile, out.toString(StandardCharsets.UTF_8), StandardOpenOption.CREATE);
 
             return result;
